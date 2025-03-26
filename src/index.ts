82b1b0f3c5f3b8a7bf392b11522e83b67ed74a71
Jayesh_Prajapati9 <jayeshprajapati.0904@gmail.com>
2025-03-26 08:58:38 +0530

    Hashing and JWT

--- src/index.ts
+++ src/index.ts
@@ -2,70 +2,75 @@ import "dotenv/config";
 import Express from "express";
 import { PrismaClient } from "@prisma/client";
 import { userMiddleware } from "./middlewares/userMiddleware";
-import crypto from "crypto"
+import crypto from "crypto";
+import Jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET || "";
 
 const app = Express();
+
 const client = new PrismaClient();
 
 app.listen(8080);
 app.use(Express.json());
 
-app.post("/api/v1/signup", async(req, res) =>{
+app.post("/api/v1/signup", async (req, res) => {
     const username = req.body.username;
     const password = req.body.passowrd;
 
-    const hashPassword = crypto.createHash("sha256").digest("hex");
-
+    const hashPassword = crypto
+        .createHash("sha256")
+        .update(password)
+        .digest("hex");
 
     const response = await client.user.create({
         data: {
             username: username,
-            password : hashPassword
-       }
-    })
+            password: hashPassword,
+        },
+    });
 
-    if (response) {
+    if (!response) {
         res.status(404).json({
-            message: "Error in DB"
+            message: "Error in DB",
         });
     }
 
     res.status(200).json({
         message: "You have signed up Succesfully",
     });
-
 });
 
-app.post("/api/v1/signin", async(req, res) =>{
+app.post("/api/v1/signin", async (req, res) => {
     const username = req.body.username;
     const password = req.body.passowrd;
 
-    const hashPassword = crypto.createHash("sha256").update(password).digest("hex");
+    const hashPassword = crypto
+        .createHash("sha256")
+        .update(password)
+        .digest("hex");
 
     const response = await client.user.findUnique({
         where: {
-            username : username
-        }
-    })
+            username: username,
+            password: hashPassword,
+        },
+    });
 
     if (!response) {
         res.status(404).json({
-            message: "You are not logged in"
+            message: "Invalid Credentials",
         });
     }
 
-    const userpassword = response?.password;
-    
-    if (userpassword == hashPassword) {
-        res.status(404).json({
-            message: "Incorrect Password",
-        });
-    }
+    const token = Jwt.sign({ id: response?.id }, JWT_SECRET);
 
     res.status(200).json({
+        token: token,
         message: "You have signed in Succesfully",
     });
-
 });
 
-
+app.post("/api/v1/content", userMiddleware, async (req, res) => {
+    
+});
